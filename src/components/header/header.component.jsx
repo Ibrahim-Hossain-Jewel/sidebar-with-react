80a203261c53695d82054de6e16f59c12a5174ac
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-01-18 23:48:46 +0600

    unnecessary dependency

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './header.styles.scss';
+const Header = ()=>(
+    <div className="header">
+        {/* This portion for Logo */}
+        <h1>I'm from component</h1>    
+        
+    </div>
+);
+export default Header;
